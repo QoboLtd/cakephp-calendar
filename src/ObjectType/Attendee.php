7bb72f04e24e9a1684be899bb0d9ff22d6ea01d8
Andrey Vystavkin <jujav4ik@gmail.com>
2018-02-16 11:41:01 +0200

    Adding attendee object and generic attendee parser (task #5394)

--- src/ObjectType/Attendee.php
+++ src/ObjectType/Attendee.php
@@ -1,9 +1,112 @@
 <?php
 namespace Qobo\Calendar\ObjectType;
 
+use Qobo\Calendar\Model\Table\Attendee as AttendeeEntity;
+
 class Attendee
 {
     protected $id;
 
-    protected $name;
+    protected $displayName;
+
+    protected $contactDetails;
+
+    protected $source;
+
+    protected $sourceId;
+
+    /**
+     * Set Id of Attendee
+     *
+     * @param mixed $id of the attendeee
+     * @return void
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed $id of the instance
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set Display Name of Attendee
+     *
+     * @param string $displayName of the attendee
+     * @return void
+     */
+    public function setDisplayName($displayName)
+    {
+        $this->displayName = $displayName;
+    }
+
+    /**
+     * @return string $displayName of attendee
+     */
+    public function getDisplayName()
+    {
+        return $this->displayName;
+    }
+
+    /**
+     * set Contact details of attendees
+     *
+     * @param mixed $contactDetails in longtext format
+     * @return void
+     */
+    public function setContactDetails($contactDetails)
+    {
+        $this->contactDetails = $contactDetails;
+    }
+
+    /**
+     * @return mixed $contactDetails of attendee instance
+     */
+    public function getContactDetails()
+    {
+        return $this->contactDetals;
+    }
+
+    /**
+     * Set Source of Attendee
+     *
+     * @param string $source of attendee instance
+     * @return void
+     */
+    public function setSource($source)
+    {
+        $this->source = $source;
+    }
+
+    /**
+     * @return string $source of attendee
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    /**
+     * Set Source Id of attendee received
+     *
+     * @param string $sourceId of attendee
+     * @return void
+     */
+    public function setSourceId($sourceId)
+    {
+        $this->sourceId = $sourceId;
+    }
+
+    /**
+     * @return string $sourceId of attendee
+     */
+    public function getSourceId()
+    {
+        return $this->sourceId;
+    }
 }
--- src/ObjectType/Parsers/Cake/AttendeeParser.php
+++ src/ObjectType/Parsers/Cake/AttendeeParser.php
@@ -6,8 +6,13 @@ use Qobo\Calendar\ObjectType\Parsers\ParserInterface;
 
 class AttendeeParser implements ParserInterface
 {
+    /**
+     * Parse Attendees Cake Entity into generic Attendee object
+     *
+     * @param \Cake\ORM\Entity $data of Attendee instance
+     * @return \Qobo\Calendar\ObjectType\Attendee $object with aggregated data
+     */
     public function parse($data)
     {
-
     }
 }
