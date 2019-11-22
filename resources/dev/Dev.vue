1e6c636809d44bd89281d4e33b6380df241c1bb9
Andrey Vystavkin <a.vystavkin@qobo.biz>
2019-11-22 13:24:43 +0200

    adding dev live reload server for isolated component tests

---
+++ resources/dev/Dev.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Modal :modal="{showModal:true, showFooter:true, title:'W00t'}"/>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import Notifications from 'vue-notification'
+import Modal from '@/components/Modal.vue'
+
+Vue.use(Notifications)
+
+export default {
+  components: {
+    Modal
+  }
+}
+</script>
